13ebe9196453cba478e68d48bd32624eb99d8023
idallas93 <Dallas.Isaac1@gmail.com>
2020-10-21 18:38:16 -0400

    deleted API file and added it to the index.js file, downloaded axios to get API to work along with generator

---
+++ ExampleREADME.md
@@ -0,0 +1,50 @@
+# read-me-generator homework
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/idallas93/undefined?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/idallas93/undefined?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  
+  ## Description 
+  
+  *The what, why, and how:* 
+  
+  Generated a readMe file using node.js and the gitHub API
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:*
+  
+  undefined
+  
+  ## Usage 
+  
+  *Instructions and examples for use:*
+  
+  undefined
+  
+  ## Tests
+  
+  *Tests for application and how to run them:*
+  
+  undefined
+  
+  ## License
+  
+  MIT
+  
+  ---
+  
+  ## Questions?
+  
+  ![Developer Profile Picture](https://avatars0.githubusercontent.com/u/10265642?v=4) 
+  
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@idallas93](https://api.github.com/users/idallas93)
+  
--- index.js
+++ index.js
@@ -1,16 +1,106 @@
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./generateMarkdown");
+const util = require('util');
+const axios = require("axios");
+
+
+
 // array of questions for user
-const questions = [
+const questions = [{
+    type: 'input',
+    name: 'username',
+    message: 'What is your GitHub username (e.g., "idallas93")?',
+},
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of your git hub repo?',
+
+},
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of your project?',
+
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Write a description of your project',
+
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license do you use?',
+    choices: ["MIT", "GNU", "N/A"]
+},
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err){
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success!")
+    })
 }
+const writeFileAsync = util.promisify(writeToFile);
 
 // function to initialize program
-function init() {
+//  function init() {
+//     inquirer.prompt(questions).then((response) => {
+//         let markDown = generateMarkdown(response)
+//         console.log(markDown);
+//         console.log(response);
+//         writeToFile("readMe.md", markDown)
 
-}
 
+//     });
+
+// }
+const api = {
+    async getUser(userResponses) {
+      try { let response = await axios
+          
+          .get(`https://api.github.com/users/${userResponses.username}`);
+          return response.data;
+  
+        } catch (error) {
+          console.log(error);
+        }
+    }
+  };
+  
+  
 // function call to initialize program
+async function init() {
+    try {
+
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you for your responses! Fetching your GitHub data next...");
+    
+        // Call GitHub api for user info
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+    
+        // Write markdown to file
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 init();
